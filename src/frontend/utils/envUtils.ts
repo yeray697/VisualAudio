5870b0652e6bd5d255579d55d3d9c97fb53d7472
Yeray <yeray.1997.yr@gmail.com>
2025-08-03 14:19:31 +0200

    Remove arm + tweaks

--- src/frontend/utils/envUtils.ts
+++ src/frontend/utils/envUtils.ts
@@ -1,6 +1,6 @@
-export const API_HOST = process.env.NEXT_PUBLIC_API_HOST || "localhost";
-export const API_PROTOCOl = process.env.NEXT_PUBLIC_API_PROTOCOL || "http";
-export const API_PORT = process.env.NEXT_PUBLIC_API_PORT || "5112";
+export const API_HOST = process.env.API_HOST || "localhost";
+export const API_PROTOCOl = process.env.API_PROTOCOL || "http";
+export const API_PORT = process.env.API_PORT || "5112";
 
 
 export const API_BASE_URL = `${API_PROTOCOl}://${API_HOST}:${API_PORT}`
