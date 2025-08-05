475bbdd0a040b4a3db1b01b6e5053011ae85b443
Yeray Ruiz <yeray.1997.yr@gmail.com>
2025-08-05 12:40:23 +0200

    Update DevNowPlayingControls.tsx

--- src/frontend/app/tv/components/DevNowPlayingControls.tsx
+++ src/frontend/app/tv/components/DevNowPlayingControls.tsx
@@ -1,7 +1,7 @@
 import { useNowPlayingStore } from "../../../store/nowPlayingStore";
 
 export const DevNowPlayingControls = () => {
-  const { overrideNowPlaying, clearOverride } = useNowPlayingStore();
+  /* const { overrideNowPlaying, clearOverride } = useNowPlayingStore();
 
   const fakeSong = {
     id: "dev-song",
@@ -15,24 +15,11 @@ export const DevNowPlayingControls = () => {
     title: "Dev Album",
     artist: "Dev Artist",
     songs: [fakeSong],
-  };
+  };*/
 
   return (
     <div>
-      <button
-        // onClick={() =>
-        //   overrideNowPlaying({
-        //     updatedAt: new Date(),
-        //     nowPlaying: fakeSong,
-        //     album: fakeAlbum,
-        //     confidence: 1,
-        //     trackPosition: 0,
-        //   })
-        // }
-      >
-        Set Fake NowPlaying
-      </button>
-      <button onClick={clearOverride}>Back to Real</button>
+
     </div>
   );
 };
