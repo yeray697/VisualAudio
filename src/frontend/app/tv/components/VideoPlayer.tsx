d9d0915a2d251c17c02ce95c481894ac305b3204
Yeray <yeray.1997.yr@gmail.com>
2025-08-08 00:35:24 +0200

    add video autoplay

--- src/frontend/app/tv/components/VideoPlayer.tsx
+++ src/frontend/app/tv/components/VideoPlayer.tsx
@@ -13,7 +13,7 @@ export const VideoPlayer = () => {
       <iframe
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/5mGuCdlCcNM"
+        src="https://www.youtube.com/embed/5mGuCdlCcNM?autoplay=1&mute=1"
         title="YouTube video"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
