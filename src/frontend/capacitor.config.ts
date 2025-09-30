b400cb5a00c5072084341e96879d3fc1c3470f12
Yeray <yeray.1997.yr@gmail.com>
2025-09-30 23:22:39 +0200

    add android apk build script

---
+++ src/frontend/capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.yeray697.visualaudio',
+  appName: 'VisualAudio',
+  webDir: 'capacitor-empty',
+  server: {
+    url: 'https://visualaudio.yeray697.dev/tv',
+  },
+};
+
+export default config;
